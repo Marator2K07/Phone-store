118cc240936ab6df85ee30e3759eda9be720b1a0
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-01 09:29:41 +0300

    Добавление контроллера AppController и его шаблона.
    Легкое изменение основного шаблона base.

---
+++ src/Controller/AppController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ManufacturerRepository;
+use App\Repository\PhoneRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class AppController extends AbstractController
+{
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    #[Route('/app', name: 'app_phone_store')]
+    public function index(PhoneRepository $phoneRepository,
+                          ManufacturerRepository $manufacturerRepository): Response
+    {
+        $phonesData = $phoneRepository->findAllAsArray();
+        $manufacturersData = $manufacturerRepository->findAllAsArray();
+        $html = $this->twig->render('index.html.twig', ['phonesData'=>$phonesData,
+                                                        'manufacturersData'=>$manufacturersData]);
+                
+        return new Response($html);
+    }
+}
