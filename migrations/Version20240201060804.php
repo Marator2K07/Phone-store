31ec1b11af3b117f036e6776b7afbfd5b19f6c84
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-01 09:09:20 +0300

    Создание миграции.
    Для подтверждения создания новых сущностей: User и Manufacturer.

---
+++ migrations/Version20240201060804.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240201060804 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE manufacturer (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE phone (id INT AUTO_INCREMENT NOT NULL, manufacturer_id_id INT NOT NULL, name VARCHAR(255) NOT NULL, battery_capacity INT NOT NULL, ram_capacity INT NOT NULL, megapixels_number INT NOT NULL, price INT NOT NULL, description VARCHAR(512) NOT NULL, image_link VARCHAR(312) NOT NULL, INDEX IDX_444F97DD741A0A47 (manufacturer_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE phone ADD CONSTRAINT FK_444F97DD741A0A47 FOREIGN KEY (manufacturer_id_id) REFERENCES manufacturer (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE phone DROP FOREIGN KEY FK_444F97DD741A0A47');
+        $this->addSql('DROP TABLE manufacturer');
+        $this->addSql('DROP TABLE phone');
+    }
+}
